1837222855c6e3e440c9ab22896aa0aa57979a03
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 21:52:27 +0530

    Refactored the code for firstName and added LastName functionality aswel

--- src/com/blz/user_registration/UserRegistrationMain.java
+++ src/com/blz/user_registration/UserRegistrationMain.java
@@ -6,7 +6,25 @@ public class UserRegistrationMain {
         System.out.println("Welcome to User Registration using regex program");
         UserRegistration validate= new UserRegistration();
         Scanner scan = new Scanner(System.in);
-        String firstName = scan.next();
-        validate.checkFirstName(firstName);
+        System.out.println("1.To Validate the First name.");
+        System.out.println("2.To Validate the Second Name.");
+        System.out.println("Enter the option number to perform the operation");
+        int option = scan.nextInt();
+        switch(option){
+            case 1:
+                System.out.println("Enter the First Name. Note: first letter should be capital");
+                String firstName = scan.next();
+                validate.validateFirstName(firstName);
+                break;
+            case 2:
+                System.out.println("2.Enter the Last name. Note: first letter should be capital");
+                String lastName = scan.next();
+                validate.validateLastName(lastName);
+                break;
+            default:
+                System.out.println("Please Enter valid option to validate the patter");
+                break;
+        }
+
     }
 }
