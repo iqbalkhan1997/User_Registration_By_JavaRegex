60ede24ec4b438d4f17121c264d13b7e9d3c0a45
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-23 19:40:12 +0530

    Added welcome message

---
+++ src/com/blz/user_registration/RegexUserRegistration.java
@@ -0,0 +1,8 @@
+package com.blz.user_registration;
+
+public class RegexUserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration using regex program");
+    }
+}
