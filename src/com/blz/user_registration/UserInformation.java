0643ff4913db7a21ab8be64e0fe4ab1d959ed1ff
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-25 20:41:59 +0530

    Builded functionality of validating email lists provided

--- src/com/blz/user_registration/UserInformation.java
+++ src/com/blz/user_registration/UserInformation.java
@@ -9,6 +9,7 @@ public class UserInformation {
     private String emailPattern;
     private String mobNumPattern;
     private String passwordPattern;
+    private String emailsListPattern;
 
     public void setFirstNamePattern(String firstNamePattern) {
         this.firstNamePattern = firstNamePattern;
@@ -40,6 +41,10 @@ public class UserInformation {
 
     public void setPasswordPattern(String passwordPattern) {    this.passwordPattern = passwordPattern; }
 
+    public String getEmailsListPattern() {    return emailsListPattern;   }
+
+    public void setEmailsListPattern(String emailsListPattern) {  this.emailsListPattern = emailsListPattern; }
+
     public void validatingInput(String input, String checkPattern, String option){
         Pattern pattern = Pattern.compile(checkPattern);
         Matcher matcher = pattern.matcher(input);
@@ -48,5 +53,14 @@ public class UserInformation {
         else
             System.out.println("Entered "+option+ " : " +input+" is invalid");
     }
-
+    public static void validateEmailsList(String[] emails) {
+        for(int i=0; i< emails.length;i++){
+            Pattern pattern = Pattern.compile("([a-z]|[a-z+-.a-z0-9])+((@[^-]+[a-z+.a-z+.a-z])|(@[^-][a-z+.a-z]))");
+            Matcher matcher = pattern.matcher(emails[i]);
+            if (matcher.matches())
+                System.out.println(emails[i]+ " : This Email id is valid");
+            else
+                System.out.println(emails[i]+ " : This Email id is Invalid");
+        }
+    }
 }
